6fac860bc097d871965f6d10f4c9b4e2f0f30df7
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-02-01 15:57:38 -0500

    feat: only show nav if not signed in

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,15 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Nav/NavBar/Navbar';
 import Header from '../components/Header/Header';
 import UserNav from '../components/UserNav/UserNav';
+import useAuth from '../hooks/useAuth';
 
 const MainLayout = () => {
+  const { auth } = useAuth();
+  console.log(auth);
   return (
     <>
       <Header>
-        <Navbar />
+        {!auth?.accessToken && <Navbar />}
         <UserNav />
       </Header>
       <Outlet />
