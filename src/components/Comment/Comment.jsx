52a1e6b6543d31e6267ceef3aca0d2b5f2a37153
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-01-31 21:05:28 -0500

    feat: admins can delete comments from all users

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,14 +1,18 @@
 import { useNavigate } from 'react-router-dom';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import StyledComment from './StyledComment';
+import { jwtDecode } from 'jwt-decode';
+import useAuth from '../../hooks/useAuth';
 
 const Comment = ({ comment, userId, postId }) => {
+  const { auth } = useAuth();
+  const decoded = auth?.accessToken ? jwtDecode(auth.accessToken) : undefined;
+  const admin = decoded?.user.admin || false;
   const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
   const handleCommentDelete = async () => {
     try {
       const res = await axiosPrivate.delete(`/comments/${comment.id}`);
-      // navigate(`/posts/${postId}`);
       navigate(0);
     } catch (err) {
       console.log(err);
@@ -20,7 +24,9 @@ const Comment = ({ comment, userId, postId }) => {
       <h5>{comment.author.username}</h5>
       <p>{comment.content}</p>
       <p>{comment.created}</p>
-      {userId === comment.author.id && <button onClick={handleCommentDelete}>Delete</button>}
+      {(userId === comment.author.id || admin) && (
+        <button onClick={handleCommentDelete}>Delete</button>
+      )}
     </StyledComment>
   );
 };
