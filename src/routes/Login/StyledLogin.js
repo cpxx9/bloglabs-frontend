9557de363768b4e5ea50d797e05b254b4b351275
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-02-02 13:18:41 -0500

    style: login and register page inset and padding

--- src/routes/Login/StyledLogin.js
+++ src/routes/Login/StyledLogin.js
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
-const StyledLogin = styled.section``;
+const StyledLogin = styled.section`
+  height: 100%;
+  padding: 1.5rem 1.5rem;
+  box-shadow: inset 0px 8px 6px -6px #888;
+`;
 
 export default StyledLogin;
--- src/routes/Register/StyledRegister.js
+++ src/routes/Register/StyledRegister.js
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
-const StyledRegister = styled.section``;
+const StyledRegister = styled.section`
+  height: 100%;
+  padding: 1.5rem 1.5rem;
+  box-shadow: inset 0px 8px 6px -6px #888;
+`;
 
 export default StyledRegister;
