3fb21403719e0a58dd6efbd4e7033e4d1acd368a
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-02-02 22:42:09 -0500

    redirects

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,16 +4,12 @@ const BASE_URL = 'https://blogapi.cjplabs.com/api';
 
 export default axios.create({
   baseURL: BASE_URL,
-  headers: {
-    'Access-Control-Allow-Origins': '*',
-  },
 });
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origins': '*',
   },
   withCredentials: true,
 });
