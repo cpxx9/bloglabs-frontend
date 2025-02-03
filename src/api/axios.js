050a25471146539d9f9690ccd5313dcd82a81dad
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-02-03 14:02:17 -0500

    url for dev backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://blogapi.cjplabs.com/api';
+const BASE_URL = 'http://localhost:8000/api';
 
 export default axios.create({
   baseURL: BASE_URL,
