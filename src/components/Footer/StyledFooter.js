d61bcae0c962beab631c1d4f69c20c0c77f4cc3d
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-02-04 19:58:16 -0500

    style: footer links and style

--- src/components/Footer/StyledFooter.js
+++ src/components/Footer/StyledFooter.js
@@ -8,7 +8,24 @@ const StyledFooter = styled.footer`
   color: #e6f3f0;
   display: flex;
   justify-content: center;
-  gap: 20px;
+
+  p:first-child {
+    padding-right: 10px;
+    border-right: 1px solid lightgray;
+  }
+
+  :nth-child(2) {
+    padding-left: 10px;
+    margin-right: 20px;
+  }
+
+  a {
+    color: inherit;
+  }
+
+  a:hover {
+    opacity: 80%;
+  }
 
   p {
     font-size: 0.8rem;
